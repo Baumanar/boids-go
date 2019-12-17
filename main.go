5eb4d275455beef74ed3145b83f9b349a2d71542
Baumanar <arnaud.baumann@phelma.grenoble-inp.fr>
2019-12-17 23:22:46 +0100

    pointer fixes

--- main.go
+++ main.go
@@ -13,16 +13,16 @@ import (
 
 
 const (
-	Acceleration_Limit = 10
-	Speed_Limit = 20
 
-	Separation_Distance = 5
-	Separation_Factor = 40
-	Alignment_Distance = 2000
-	Alignment_Factor = 2000
-	Cohesion_Distance = 12
-	Cohesion_Factor = 10
+	Acceleration_Limit = 5
+	Speed_Limit = 10
 
+	Separation_Distance = 0.01
+	Separation_Factor = 1
+	Alignment_Distance = 50
+	Alignment_Factor = 3
+	Cohesion_Distance = 100
+	Cohesion_Factor = 3
 )
 
 
@@ -31,9 +31,9 @@ var (
 
 	window_width = 1024
 	window_height = 768
-	World_Size_Width = 1200.0
-	World_Size_Height = 800.0
-	population_size = 1
+	World_Size_Width = 1400.0
+	World_Size_Height = 1200.0
+	population_size = 50
 
 )
 
@@ -78,6 +78,7 @@ func (b *Boid) boids_near (otherBoids []*Boid, distance_min float64) []*Boid{
 
 func (b *Boid) limitVelo(){
 	square_speed := norm(b.velocity)
+	fmt.Println(square_speed)
 	if square_speed > ( Speed_Limit ) {
 		b.velocity = b.velocity.Scaled( Speed_Limit / math.Sqrt( square_speed ) )
 	}
@@ -88,36 +89,43 @@ func (b *Boid) updateBoid (otherBoids []*Boid, outBoid chan *Boid){
 	updatedBoid := new(Boid)
 	*updatedBoid = *b
 	var acceleration pixel.Vec
+	fmt.Println("last position: ", updatedBoid.position)
 
 
-	boidsNear := b.boids_near(otherBoids, 2000)
+	boidsNear := b.boids_near(otherBoids, 200)
+	//fmt.Println("number of near boids: ", len(boidsNear))
 	for _, bNear := range boidsNear{
 			diff_vector := b.diff_vector(bNear)
-			fmt.Println("diff vector: ", diff_vector)
+			//fmt.Println("diff vector: ", diff_vector)
 			distance := b.distance_to(bNear)
+		//fmt.Println("distance: ", distance)
 			if distance < Separation_Distance {
-				acceleration.Add( diff_vector.Scaled( -Separation_Factor ) )
+
+				acceleration = acceleration.Add( diff_vector.Scaled( Separation_Factor ) )
 			}
 
 			if distance < Alignment_Distance {
-				acceleration.Add( diff_vector.Scaled( Cohesion_Factor ) )
+				acceleration = acceleration.Add( diff_vector.Scaled( -Cohesion_Factor ) )
 			}
 
 			if distance < Alignment_Distance {
-				acceleration.Add( bNear.velocity.Scaled( Alignment_Factor ) )
+				acceleration = acceleration.Add( bNear.velocity.Scaled( -Alignment_Factor ) )
 			}
 
 			square_acceleration_magnitude := norm(acceleration)
-			if ( square_acceleration_magnitude > ( Acceleration_Limit * Acceleration_Limit ) ) {
+
+			if ( square_acceleration_magnitude > (  Acceleration_Limit ) ) {
 				acceleration = acceleration.Scaled( Acceleration_Limit / math.Sqrt( square_acceleration_magnitude ) )
 			}
 
-
 	}
-	updatedBoid.velocity.Add( acceleration )
+	//fmt.Println("acc: ", acceleration)
+	//fmt.Println("speed: ", updatedBoid.velocity)
+
+	updatedBoid.velocity = updatedBoid.velocity.Add( acceleration )
 	updatedBoid.limitVelo()
-	updatedBoid.position.Add( updatedBoid.velocity)
-	fmt.Println("new position: ", updatedBoid.position)
+	updatedBoid.position = updatedBoid.position.Add( updatedBoid.velocity)
+	fmt.Println("\nnew position: ", updatedBoid.position, updatedBoid.velocity)
 
 	for updatedBoid.position.X < 0 {
 		updatedBoid.position.X += World_Size_Width
@@ -134,40 +142,42 @@ func (b *Boid) updateBoid (otherBoids []*Boid, outBoid chan *Boid){
 	for updatedBoid.position.Y > World_Size_Height {
 		updatedBoid.position.Y -= World_Size_Height
 	}
-	fmt.Println("in update", updatedBoid.position)
 	outBoid <- updatedBoid
 }
 
 
 func create_boid () *Boid {
 	new_boid := new( Boid )
-	new_boid.position.X = rand.Float64() * World_Size_Width - 200;
-	new_boid.position.Y = rand.Float64() * World_Size_Height - 200;
+	new_boid.position.X = rand.Float64() * World_Size_Width - 400;
+	new_boid.position.Y = rand.Float64() * World_Size_Height - 400;
+	new_boid.velocity.X = rand.Float64() * 2 ;
+	new_boid.velocity.Y = rand.Float64() * 2 ;
 	new_boid.last_updated = time.Now()
 	new_boid.color = pixel.RGB(1, 0, 0)
 
 	return new_boid
 }
 
-func loop_state(last_state []*Boid,  state_channel chan []*Boid) {
-	fmt.Println("started updating")
-	boid_updates := make( chan *Boid, 1000 )
-	for running {
+func loop_state(last_state []*Boid,  state_channel chan []*Boid, boid_updates chan *Boid) {
+	//fmt.Println("started updating")
 		new_state := make( []*Boid, population_size )
-
-		for i := 0; i < len( new_state ); i++ {
-			fmt.Println(last_state[i].position)
-			if last_state != nil && len( last_state ) > i {
+		fmt.Println("in loop state: ")
+		fmt.Println("\t before update: ", last_state[0].position)
+		for i := 0; i < len( last_state ); i++ {
+			//fmt.Println(last_state[i].position)
+			//if last_state != nil && len( last_state ) > i {
 				//fmt.Println( last_state[ i ].velocity.X, last_state[ i ].velocity.Y)
 				go last_state[ i ].updateBoid(last_state, boid_updates )
+			//}
+		}
+		for i := 0; i < len( last_state ); i++ {
+			x, ok := <-boid_updates
+			if ok{
+				new_state[i] = x
 			}
 		}
-		for i := 0; i < len( new_state ); i++ {
-			new_state[ i ] = <- boid_updates
-		}
 		state_channel <- new_state
-		last_state = new_state
-	}
+
 }
 
 
@@ -186,13 +196,20 @@ func run() {
 	for i:=0; i<population_size; i++ {
 		last_state = append(last_state, create_boid())
 	}
+	fmt.Println("at start:", last_state[0].position)
 	state_channel := make( chan []*Boid )
-
+	boid_updates := make( chan *Boid, 1 )
 	for !win.Closed() {
 		win.Clear(colornames.White)
-		go loop_state(last_state, state_channel)
-		draw_state( <- state_channel, win )
+		go loop_state(last_state, state_channel, boid_updates)
+		x, ok := <- state_channel
+		if ok {
+			last_state = x
+			fmt.Println("got value", x[0].position)
+			draw_state( x, win )
 
+		}
+		time.Sleep(50 * time.Millisecond)
 		win.Update()
 	}
 }
@@ -203,10 +220,8 @@ func main() {
 }
 
 func draw_state( state []*Boid, win *pixelgl.Window) {
-	fmt.Println("started drawing")
 
 	for _, boid := range state {
-		fmt.Println("reading", boid.position)
 		boid.drawBoid(win)
 	}
 
@@ -214,12 +229,20 @@ func draw_state( state []*Boid, win *pixelgl.Window) {
 
 
 func (b *Boid) drawBoid(win *pixelgl.Window) {
-
+	mat := pixel.IM
 	imd := imdraw.New(nil)
 	imd.Color = b.color
 	imd.Push(b.position)
-	imd.Push(b.position.Add(pixel.V(20,20)))
-	imd.Push(b.position.Sub(pixel.V(20,20)))
-	imd.Polygon(20)
+	fmt.Println("in draw pos: ", b.position)
+	vec := b.position.Sub(pixel.V(-20, 7))
+	imd.Push(vec)
+	vec = b.position.Add(pixel.V(20, 7))
+	//imd.Push(pixel.V(600, 600))
+	imd.Push(vec)
+	mat = mat.Rotated(b.position,  math.Atan2( b.velocity.X, b.velocity.Y ))
+	imd.Color = b.color
+	imd.SetMatrix(mat)
+	imd.Polygon(0)
 	imd.Draw(win)
+
 }
